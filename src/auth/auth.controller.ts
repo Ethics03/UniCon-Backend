8dc10cab5e9d0b842f916bc25bd5cb9a2b9bd722
Rachit Srivastava <honey032004@gmail.com>
2024-11-23 21:17:08 +0530

    AddedDTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,10 @@
 import { Controller,Post, Get , Body} from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  
+  @Post('login')
+    login(@Body() data){
 
+}
+}
