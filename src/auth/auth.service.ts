5d71bc0ee8eb74c15931965bb8521f5f244c28f2
Rachit Srivastava <honey032004@gmail.com>
2024-11-27 01:39:36 +0530

    feat: UpdateUser Service added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { ConflictException, Injectable , NotFoundException, Param, UnauthorizedException} from '@nestjs/common';
+import { ConflictException, Injectable , NotFoundException, Param, UnauthorizedException , InternalServerErrorException} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { AuthPayloadDTO, AuthResponseDTO , CreateUserDTO} from './dto/auth.dto';
+import { AuthPayloadDTO, AuthResponseDTO , CreateUserDTO, UpdateUserDTO} from './dto/auth.dto';
 import { JwtPayload } from './jwt-payload.interface';
 import { NotFoundError } from 'rxjs';
 
@@ -14,6 +14,7 @@ export class AuthService {
     ){}
 
 
+    //VALIDATING USER
     async validateUser(logindto: AuthPayloadDTO): Promise<any>{
         const {username,password} = logindto
 
@@ -27,6 +28,8 @@ export class AuthService {
         }
 
 
+
+    //CREATING USER
     async createUser(createdata: CreateUserDTO): Promise<AuthResponseDTO>{
 
         const {username,email} = createdata;
@@ -34,19 +37,17 @@ export class AuthService {
         const existingemail = await this.prisma.users.findUnique({where: {email}});
 
         if(existingusername){
-            throw new ConflictException('Username already exists. Please choose a different username.');
+            throw new ConflictException('Username already taken. Please try again.');
         }
         if(existingemail){
-            throw new ConflictException('An account already exists for this email');
+            throw new ConflictException('Email already registered. Please try again.');
         }
-
-
-
 
     try{
         
+        const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS, 10) || 10;
 
-        const hashedpass = await bcrypt.hash(createdata.password,10);
+        const hashedpass = await bcrypt.hash(createdata.password,saltRounds);
            const newUser = await this.prisma.users.create({
             data: {
                 ...createdata,
@@ -57,24 +58,30 @@ export class AuthService {
         const token = this.jwtService.sign({
             username: newUser.username,
             sub: newUser.id,
-        })
+        },
+        {
+            secret: process.env.JWT_SECRET_KEY, 
+            expiresIn: '6h', 
+          })
 
         return {
             access_token: token,
     }
     }
     catch (error) {
-        throw new Error("Failed to create user. Please try again later.");
+        throw new InternalServerErrorException('Failed to create user. Please try again later.');
     }
     }
 
 
-    async findUserByUsernameAndId(username: string, userId: number) {
+    async findUserById(userId: number) {
         return await this.prisma.users.findUnique({
-            where: { username, id: userId }, // Ensure you match both username and userId
+            where: {id: userId }, // Ensure you match both username and userId
         });
     }
     
+
+    //DELETING USER
     async deleteUser(userId: number) {
         
         const user =  await this.prisma.users.findUnique({
@@ -90,10 +97,61 @@ export class AuthService {
           return { message: 'User deleted successfully' };
     }
 
-    
+    //UPDATING USER
+    async updateUser(userId: number, updatedata: UpdateUserDTO): Promise<AuthResponseDTO>{
+        const user = await this.prisma.users.findUnique({
+            where: {id: userId},
+        });
+
+        if(!user){
+            throw new NotFoundException('User not found.')
+        }
+        const existingUsername = await this.prisma.users.findUnique({
+            where: { username: updatedata.username },
+        });
+        const existingEmail = await this.prisma.users.findUnique({
+            where: { email: updatedata.email },
+        });
+
+        if(existingUsername && existingUsername.id != userId){
+            throw new ConflictException('Username is already taken.');
+        }
+        if(existingEmail && existingEmail.id != userId){
+            throw new ConflictException('Email is already taken.');
+        }
+
+    try{
+        const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS, 10) || 10;
+        if(updatedata.password){
+            updatedata.password = await bcrypt.hash(updatedata.password,saltRounds);
+        }
+
+        const updatedUser = await this.prisma.users.update({
+            where: {id: userId},
+            data: updatedata
+        });
+
+        const UpdatedUsertoken = this.jwtService.sign({
+            username: updatedUser.username,
+            sub: updatedUser.id
+        },
+        {
+            secret: process.env.JWT_SECRET_KEY,
+            expiresIn: '6h',
+        });
+        return{
+            access_token: UpdatedUsertoken
+        };
+    }
+    catch(error){
+        throw new InternalServerErrorException('Error Updating. Try Again');
+    }
+        
+    }
 
 
 
+    //LOGGING IN AND CREATING TOKEN
     async login(payload : AuthPayloadDTO): Promise<AuthResponseDTO>{
 
         const user = await this.validateUser(payload)
@@ -102,7 +160,11 @@ export class AuthService {
         const token = this.jwtService.sign({
             username: user.username,
             sub: user.id,
-        })
+        },
+        {
+            secret: process.env.JWT_SECRET_KEY,
+            expiresIn: '6h', 
+          })
         
         return {access_token: token}
     }
