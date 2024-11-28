003bd94ad55beb2b79602cc2ba1f01ab550732db
Rachit Srivastava <honey032004@gmail.com>
2024-11-29 01:26:20 +0530

    feat: Added GoogleAuthGuard

--- src/auth/google-OAuth.guard.ts
+++ src/auth/google-OAuth.guard.ts
@@ -1,5 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
-
-export class GoogleOAuthGuard extends AuthGuard('google'){}
+//auth guard middleware
+//THIS IS WHAT HELPS US TO REDIRECT TO THE GOOGLE AUTH PAGE
+@Injectable()
+export class GoogleOAuthGuard extends AuthGuard('google'){
+    async canActivate(context: ExecutionContext) {
+        const activate = (await super.canActivate(context)) as boolean;
+        const request = context.switchToHttp().getRequest();
+        await super.logIn(request);
+        return activate;
+    }
+}
