65051130a1ed087a22f9eaac69c2f156217348cd
Rachit Srivastava <honey032004@gmail.com>
2024-11-29 23:29:19 +0530

    feat: Added Express-session

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser()); //enabled cookie parser middleware
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+     cookie: {
+        httpOnly: true,  // helps mitigate XSS attacks
+        maxAge: 1000 * 60 * 60 * 6, // expires after 6 hours
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
