814c0b6f571caff6af9a30552ed35cc47e19bc1a
Rachit Srivastava <honey032004@gmail.com>
2024-12-02 23:43:11 +0530

    feat: added mailgun module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
@@ -10,11 +10,19 @@ import { ConfigModule } from '@nestjs/config';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { GoogleOAuthGuard } from './google-OAuth.guard';
 import { SessionSerializer } from './Serializer';
+import { MailgunModule  } from 'nestjs-mailgun';
+
 
 @Module({
   imports: [
+    
     ConfigModule.forRoot({isGlobal:true}),
-
+    MailgunModule.forRoot({
+      username: 'api',
+      key: process.env.MAILGUN_API_KEY,
+      timeout: 30000,
+      url: "https://api.mailgun.net/v3/sandbox96f735e344b1489e85ff4708efa12c16.mailgun.org/messages", // or your own Mailgun domain
+    }),
     PrismaModule,
     PassportModule.register({session: true}),
     JwtModule.register({
@@ -22,8 +30,11 @@ import { SessionSerializer } from './Serializer';
       signOptions: {expiresIn: '6h'},
 
     }),
+    
   ],
+ 
   controllers: [AuthController],
   providers: [AuthService,JwtStrategy,PrismaService,GoogleStrategy,GoogleOAuthGuard,SessionSerializer],
+  
 })
 export class AuthModule {}
