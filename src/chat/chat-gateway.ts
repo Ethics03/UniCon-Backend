5f782113c31fa3566db3efb55fbbb6122dfbc7eb
Rachit Srivastava <honey032004@gmail.com>
2025-01-01 17:03:53 +0530

    feat:Message Broadcasting added

--- src/chat/chat-gateway.ts
+++ src/chat/chat-gateway.ts
@@ -1,10 +1,37 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
 
 
-@WebSocketGateway()
-export  class ChatGateway {
+@WebSocketGateway({cors: {origin: '*'}})
+export  class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
+
+    @WebSocketServer() server: Server;    
+
+    handleConnection(client: Socket) {
+        console.log("New user connected..",client.id)
+
+        client.broadcast.emit('user-joined',{
+            message: `New User Joined the chat: ${client.id}`, 
+        });
+    }
+
+    handleDisconnect(client: Socket) {
+        console.log("User Disconnected..",client.id)
+
+        this.server.emit('user-left',{
+            message: `User Left the chat: ${client.id}`,
+        });
+    }
+
     @SubscribeMessage("MessageEvent")
-    handleNewMessage(@MessageBody() mes: any){
-            console.log(mes)
+    handleNewMessage(@MessageBody() message: string){
+
+            this.server.emit('message',message); //broadcast the message to all
     }
 }
+
+//socket.on()
+
+//socket.emit() -> emitting or replying 
+
+//io.emit() -> broadcasting message to all the users
