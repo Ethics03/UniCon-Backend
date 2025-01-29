36271c71853c0efed8bb20fcec7029e18d36bb8b
Rachit Srivastava <honey032004@gmail.com>
2025-01-30 02:19:53 +0530

    Added: logger

--- src/chat/chat-gateway.ts
+++ src/chat/chat-gateway.ts
@@ -1,13 +1,16 @@
 import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
-
+import { Logger } from "@nestjs/common";
 
 @WebSocketGateway({cors: {origin: '*'}})
 export  class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
 
-    @WebSocketServer() server: Server;    
+    @WebSocketServer() 
+    server: Server;    
 
-    handleConnection(client: Socket) {
+    private logger = new Logger('ChatGateway');
+
+    handleConnection(client: Socket) { //handling connection
         console.log("New user connected..",client.id)
 
         client.broadcast.emit('user-joined',{  //broadcasting to all the users except the user who joined
@@ -15,7 +18,9 @@ export  class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
         });
     }
 
-    handleDisconnect(client: Socket) {
+    
+    handleDisconnect(client: Socket) { //handling disconnect event
+
         console.log("User Disconnected..",client.id)
 
         this.server.emit('user-left',{
@@ -25,7 +30,9 @@ export  class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
 
     @SubscribeMessage("MessageEvent")
     handleNewMessage(@MessageBody() message: string){
+            this.logger.log(`Message recieved: ${message}`);
             this.server.emit('message',message); //broadcast the message to all
+            return message;
     }
 }
 
